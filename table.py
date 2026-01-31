663e3e85de504deb6b4bbcfb704efdc6aeeb6112
Mohammad Ayan <ayanchaudhart@gmail.com>
2026-01-31 19:31:03 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,19 @@
+   #   TABLE
+ 
+n=eval(input("Enter any number :"))
+for i in range (1,11):
+    i=i*n
+    print(i)
+
+  # SUM OF ODD NUMBERS
+
+sum = 0    
+for i in range(1,101):
+   if i % 2 != 0:
+       sum = sum+i
+       
+print(sum)   
+
+  
+
+           
